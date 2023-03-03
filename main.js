7a34addcacdef703dd6ed1dce1db757dd4fb0875
joshua jensen <joshjj3@gmail.com>
2023-03-03 16:13:39 -0700

    int commit

---
+++ main.js
@@ -0,0 +1,87 @@
+// main clicking loop
+//levelUp function that handles incrementing the hero level checking for gold and cost increment
+// dps interval function
+let gold = 0
+let dps = 0
+let clickDamage = 0
+heroes = [
+    {
+        name: 'cid',
+        lvl: 0,
+        dps: 0,
+        baseDpsIncrease: 0,
+        clickDamageAdded: 0,
+        clickDamageIncrease: 1,
+        cost: 5,
+    },
+    {
+        name: 'treebeast',
+        lvl: 0,
+        dps: 1,
+        baseDpsIncrease: 1,
+        clickDamageAdded: 0,
+        clickDamageIncrease: ,
+        cost: 50,
+    },
+]
+drawGold()
+drawHeroes()
+
+function gameLoop() {
+    gold += (clickDamage + 1)
+    console.log(gold)
+    drawGold()
+}
+
+function drawDamage() {
+
+}
+
+function drawGold() {
+    let goldElem = document.getElementById('gold')
+    goldElem.innerText = gold.toString()
+}
+
+function levelUp(name) {
+    heroes.forEach(h => {
+        if (h.name == name) {
+            if (gold >= h.cost) {
+                h.lvl++
+                h.dps += baseDpsIncrease
+                h.clickDamageAdded += h.clickDamageIncrease
+                clickDamage += h.clickDamageAdded
+                dps += h.dps
+                gold -= h.cost
+                h.cost = Math.floor(h.cost * (1.07 ** h.lvl))
+                drawLevelUp(h.name)
+                drawGold()
+                console.log(h)
+            }
+        }
+    });
+}
+
+function drawLevelUp(name) {
+    heroes.forEach(h => {
+        if (h.name == name) {
+            let levelElem = document.getElementById(h.name)
+            let cost = levelElem.querySelector('.cost')
+            let textLevel = levelElem.querySelector('.lvl')
+            textLevel.innerText = h.lvl.toString()
+            cost.innerText = h.cost.toString()
+            console.log(h)
+        }
+    })
+
+}
+
+function drawHeroes() {
+    heroes.forEach(h => {
+        let levelElem = document.getElementById(h.name)
+        console.log(levelElem)
+        let cost = levelElem.querySelector('.cost')
+        let textLevel = levelElem.querySelector('.lvl')
+        cost.innerText = h.cost.toString()
+        textLevel.innerText = h.lvl.toString()
+    })
+}
